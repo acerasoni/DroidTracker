5bf31a0ab9191e99f0e8eeb6442b32e3bd4425ff
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-12-14 07:59:48 +0000

    Adding GPS tracking code

--- app/src/main/java/com/ltm/runningtracker/activity/MainActivity.java
+++ app/src/main/java/com/ltm/runningtracker/activity/MainActivity.java
@@ -1,18 +1,47 @@
 package com.ltm.runningtracker.activity;
 
+import android.Manifest;
+import android.Manifest.permission;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.ltm.runningtracker.R;
+import com.ltm.runningtracker.util.TrackerLocationListener;
 
 public class MainActivity extends AppCompatActivity {
+
+  LocationManager locationManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    locationManager =
+        (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//
+//    ActivityCompat.requestPermissions(this,
+//        new String[]{permission.ACCESS_FINE_LOCATION},
+//        0);
+
+  }
+
+  public void onClick(View v) {
+
+    TrackerLocationListener locationListener = new TrackerLocationListener();
+    try {
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+          3, // minimum time interval between updates
+          1, // minimum distance between updates, in metres
+          locationListener);
+    } catch (SecurityException e) {
+   //  Log.d("g53mdp", e.toString());
+    }
   }
 }
